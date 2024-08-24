4a208987e31917eb2589854428a52bfce052953e
Web Artisan <ad4ptec@gmail.com>
2024-08-24 18:38:27 +0500

    optimize auth requests
    increases the performance of HTTP requests requiring sanctum authentication

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -3,28 +3,20 @@
 namespace App\Models;
 
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
     /**
-     * @param array $options
-     * @return void
+     * Update the last_used_at field no more than 1 time per minute.
+     * This change increases the performance of HTTP requests requiring sanctum authentication.
      */
-    public function save(array $options = []): void
+    protected function lastUsedAt(): Attribute
     {
-        $changes = $this->getDirty();
-
-        /**
-         * Update the last_used_at field no more than 1 time per minute.
-         * This change increases the performance of HTTP requests requiring sanctum authentication.
-         */
-        if (
-            !array_key_exists('last_used_at', $changes) ||
-            count($changes) > 1 ||
-            !$this->getOriginal('last_used_at') ||
-            $this->getOriginal('last_used_at') < now()->parse($changes['last_used_at'])->subMinute()
-        ) {
-            parent::save();
-        }
+        return Attribute::make(
+            set: fn (string $value) => $this->getOriginal('last_used_at') < now()->parse($value)->subMinute()
+                ? $value
+                : $this->getOriginal('last_used_at'),
+        );
     }
 }
