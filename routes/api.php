30b429f9d72c3cede4055419a518b2e211d29b8e
Web Artisan <ad4ptec@gmail.com>
2025-07-20 21:45:06 +0500

    refactor: implement auth guard-specific routing

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::prefix('api/v1')->group(function () {
     Route::get('login/{provider}/redirect', [AuthController::class, 'redirect'])->name('login.provider.redirect');
-    Route::get('login/{provider}/callback', [AuthController::class, 'callback'])->name('login.provider.callback');
+    Route::get('login/{provider}/callback', [AuthController::class, 'callback'])->middleware(config('auth.defaults.guard'))->name('login.provider.callback');
     Route::post('login', [AuthController::class, 'login'])->middleware('throttle:login')->name('login');
     Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('forgot-password', [AuthController::class, 'sendResetPasswordLink'])->middleware('throttle:5,1')->name('password.email');
@@ -19,7 +19,7 @@ Route::prefix('api/v1')->group(function () {
     Route::post('verification-notification', [AuthController::class, 'verificationNotification'])->middleware('throttle:verification-notification')->name('verification.send');
     Route::get('verify-email/{ulid}/{hash}', [AuthController::class, 'verifyEmail'])->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
 
-    Route::middleware(['auth'])->group(function () {
+    Route::middleware(["auth:" . config('auth.defaults.guard')])->group(function () {
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
         Route::post('devices/disconnect', [AuthController::class, 'deviceDisconnect'])->name('devices.disconnect');
         Route::get('devices', [AuthController::class, 'devices'])->name('devices');
