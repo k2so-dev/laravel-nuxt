b553c5017ace14ae2f5f58aa40721772c67c6a1c
Web Artisan <ad4ptec@gmail.com>
2026-05-05 15:51:08 +0300

    chore: migrate phpunit to pest

--- apps/api/tests/TestCase.php
+++ apps/api/tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Routing\Middleware\ThrottleRequestsWithRedis;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware([
+            PreventRequestForgery::class,
+            ThrottleRequests::class,
+            ThrottleRequestsWithRedis::class,
+        ]);
+    }
 }
